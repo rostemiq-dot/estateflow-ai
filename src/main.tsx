52b6eb19a8357df30cb67d4394d81912705d1889
mohammed rostam <rostem.iq@gmail.com>
2026-09-02 09:18:03 +0300

    Make agency identity and settings reactive

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AppProviders } from "./app/providers";
 import { router } from "./app/router";
+import { applySettingsTheme, loadSettings } from "./features/settings/settings-storage";
 import "./index.css";
+
+applySettingsTheme(loadSettings());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
