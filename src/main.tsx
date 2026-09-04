b54eca3d42ff6f967b77ecf7fdbbb47eaf8d21f9
mohammed rostam <rostem.iq@gmail.com>
2026-09-04 12:35:49 +0300

    feat: add global UI error recovery

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { RouterProvider } from "react-router-dom";
 import { AppProviders } from "./app/providers";
 import { router } from "./app/router";
 import { applySettingsTheme, loadSettings } from "./features/settings/settings-storage";
+import { AppErrorBoundary } from "./components/ui/AppErrorBoundary";
 import "./index.css";
 
 applySettingsTheme(loadSettings());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
+    <AppErrorBoundary>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+    </AppErrorBoundary>
   </StrictMode>,
 );
