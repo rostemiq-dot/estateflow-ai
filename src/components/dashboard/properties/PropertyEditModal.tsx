3c91517b7f8a86cfa69b6d44122577b46ccd99ed
mohammed rostam <rostem.iq@gmail.com>
2026-09-06 01:12:04 +0300

    support async property editing

--- src/components/dashboard/properties/PropertyEditModal.tsx
+++ src/components/dashboard/properties/PropertyEditModal.tsx
@@ -1,23 +1,8 @@
 import type { Property } from "../../../features/properties/property-data";
 import { PropertyEditorModal } from "./PropertyEditorModal";
 
-type PropertyEditModalProps = {
-  property: Property;
-  onClose: () => void;
-  onSave: (property: Property) => boolean;
-};
+type PropertyEditModalProps = { property: Property; onClose: () => void; onSave: (property: Property) => boolean | Promise<boolean> };
 
-export function PropertyEditModal({
-  property,
-  onClose,
-  onSave,
-}: PropertyEditModalProps) {
-  return (
-    <PropertyEditorModal
-      mode="edit"
-      property={property}
-      onClose={onClose}
-      onSave={onSave}
-    />
-  );
+export function PropertyEditModal({ property, onClose, onSave }: PropertyEditModalProps) {
+  return <PropertyEditorModal mode="edit" property={property} onClose={onClose} onSave={onSave} />;
 }
