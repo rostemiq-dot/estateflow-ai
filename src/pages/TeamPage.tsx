13f525af98e13a7e4e67e168c46333d233492b78
mohammed rostam <rostem.iq@gmail.com>
2026-09-19 10:21:27 +0300

    Prepare database pages for hook dependency lint

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,5 +1,5 @@
 import { Mail, RefreshCw, ShieldCheck, Users } from "lucide-react";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { DashboardShell } from "../components/dashboard/DashboardShell";
 import { DatabasePageSkeleton } from "../components/ui/DatabasePageSkeleton";
 import { useToast } from "../components/ui/ToastProvider";
